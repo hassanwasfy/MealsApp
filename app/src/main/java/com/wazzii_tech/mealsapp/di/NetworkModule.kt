ef96f2781920333a4996cbfdf560a6bc2b5d5075
Hassan Wasfy <wasfyelgamed@gmail.com>
2023-03-08 14:38:24 +0200

    finish networkModule

--- app/src/main/java/com/wazzii_tech/mealsapp/di/NetworkModule.kt
+++ app/src/main/java/com/wazzii_tech/mealsapp/di/NetworkModule.kt
@@ -1,5 +1,32 @@
 package com.wazzii_tech.mealsapp.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    fun provideOkHttp():OkHttpClient{
+        return OkHttpClient()
+            .newBuilder()
+            .connectTimeout(20,TimeUnit.SECONDS)
+            .readTimeout(20,TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("www.themealdb.com/api/json/v1/1/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
